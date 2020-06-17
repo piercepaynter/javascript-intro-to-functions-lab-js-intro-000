a7ec82ecec22280682c3f5d144eead9532f6f7cc
boisterous-puppet-2200 <paynterpierce@gmail.com>
2020-06-17 04:25:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  var lowercase = "string"
-  if (string.toLowerCase() === lowercase) {
+  if (string === string.toLowerCase()) {
     return "I can\'t hear you!";
   }
 }
