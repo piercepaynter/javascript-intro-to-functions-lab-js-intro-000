3d85401d94111778567c793d911c592844f738f8
boisterous-puppet-2200 <paynterpierce@gmail.com>
2020-06-17 04:28:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,8 +13,11 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can\'t hear you!";
-  if (string === string.toUpperCase()) {
+  else if (string === string.toUpperCase()) {
     return "YES INDEED!"
+  else if (string === "I love you, too.") {
+    return "I love you too, Grandma."
+  }
   }
   }
 }
