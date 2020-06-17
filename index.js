42c1c50410881401592fe77a8006947c0642f376
boisterous-puppet-2200 <paynterpierce@gmail.com>
2020-06-17 04:33:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function sayHiToGrandma(string) {
     return "I can\'t hear you!";
   else if (string === string.toUpperCase()) {
     return "YES INDEED!";
-  else if (string === "I love you, too.") {
-    return "I love you too, Grandma.";
+  else if (string === "I love you, Grandma.") {
+    return "I love you too.";
   }
   }
   }
